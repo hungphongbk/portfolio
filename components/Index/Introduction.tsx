8980c95cd4b504bc6ef4382bc9f235c3d5b1d011
hungphongbk <programmingd32@gmail.com>
2021-06-02 22:49:48 +0700

    add Introduction.tsx

--- components/Index/Introduction.tsx
+++ components/Index/Introduction.tsx
@@ -1,6 +1,7 @@
 import {makeStyles} from "@material-ui/core/styles";
 import SectionHeader from "./SectionHeader";
 import BubbleArtwork from '../../assets/artworks/Bubble.svg'
+import {Typography} from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -8,16 +9,47 @@ const useStyles = makeStyles(theme => ({
     marginTop: -1,
     '& svg': {
       height: '75vh',
-      width: 'auto'
+      width: 'auto',
+      marginRight: '5%'
     },
     position: 'relative',
     display: 'flex',
-    justifyContent: 'end'
+    justifyContent: 'end',
+    '& ul': {
+      position: 'absolute',
+      top: '20vh',
+      left: '5%',
+      listStyle: 'outside none none',
+      '& .MuiTypography-body1': {
+        color: '#fafafa',
+        paddingTop: theme.spacing(1),
+        paddingBottom: theme.spacing(1),
+      },
+      '& li': {
+        position: 'relative',
+        paddingLeft: theme.spacing(3),
+        '&::before': {
+          position: 'absolute',
+          content: "'▹'",
+          left: 0,
+          color: '#fafafa',
+          fontSize: '1.7rem'
+        }
+      }
+    }
   },
   content: {
     position: 'absolute',
     top: '15vh',
-    right: '24vh'
+    right: 'calc(24vh + 5%)',
+    textAlign: 'right',
+    width: '60vh',
+    '& .MuiTypography-body1': {
+      lineHeight: 1.7,
+      '& strong': {
+        color: theme.palette.primary.main
+      }
+    }
   }
 }))
 
@@ -26,7 +58,19 @@ export default function Introduction() {
   return <div className={classes.root}>
     <BubbleArtwork/>
     <div className={classes.content}>
-      <SectionHeader number={1} title={"About me"}/>
+      <SectionHeader number={1} title={"About me"} align={"end"}/>
+      <Typography>At first, my name is <strong>Truong Hung Phong</strong>, HCMC-based frontend developer with 5 years
+        experience.
+        Currently I've had the privilege of working at a startup and several e-commerce companies. I'm quietly
+        confident,
+        naturally curious, and perpetually working on improving my chops one coding problem at a time.</Typography>
     </div>
+    <ul>
+      <li><Typography>ReactJS & NextJS</Typography></li>
+      <li><Typography>JavaScript (ES6+) & Typescript</Typography></li>
+      <li><Typography>NodeJS</Typography></li>
+      <li><Typography>MongoDB, MySQL, PostgreSQL</Typography></li>
+      <li><Typography>AWS</Typography></li>
+    </ul>
   </div>
 }
--- components/Index/SectionHeader.tsx
+++ components/Index/SectionHeader.tsx
@@ -1,27 +1,31 @@
 import {ReactNode} from "react";
 import {makeStyles} from "@material-ui/core/styles";
+import {Theme as DefaultTheme} from "@material-ui/core/styles/createMuiTheme";
 import {Typography} from "@material-ui/core";
 
-const useStyles = makeStyles(theme => ({
+type SectionHeaderProps = {
+  number: number,
+  title: ReactNode,
+  align?: 'start' | 'center' | 'end'
+}
+
+const useStyles = makeStyles<DefaultTheme, SectionHeaderProps>(theme => ({
   root: {
     display: 'flex',
-    justifyContent: 'center'
+    justifyContent: props => props.align ?? 'center',
+    marginBottom: theme.spacing(2)
   },
   title: {
     '& small': {
       color: theme.palette.primary.main,
-      display:'inline-block',
-      marginRight:'.7rem'
+      display: 'inline-block',
+      marginRight: '.7rem'
     }
   }
 }))
 
-type SectionHeaderProps = {
-  number: number,
-  title: ReactNode
-}
 export default function SectionHeader(props: SectionHeaderProps) {
-  const classes = useStyles();
+  const classes = useStyles(props);
   return <div className={classes.root}>
     <Typography variant={'h4'} className={classes.title}>
       <small>{`${(props.number + "").padStart(2, '0')}.`}</small>
