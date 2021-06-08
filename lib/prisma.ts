22c6ea9140b1f153540b28d444f4400a00f8d4eb
hungphongbk <programmingd32@gmail.com>
2021-06-08 14:12:38 +0700

    upgrade to webpack@5

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,23 @@
-import { PrismaClient } from '@prisma/client'
+import {PrismaClient} from "@prisma/client";
 
-let prisma: PrismaClient
+import Adapters from "next-auth/adapters";
 
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient()
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient()
+const prismaClientPropertyName = `__prevent-name-collision__prisma`;
+type GlobalThisWithPrismaClient = typeof globalThis & {
+  [prismaClientPropertyName]: PrismaClient;
+};
+
+const getPrismaClient = () => {
+  if (process.env.NODE_ENV === `production`) {
+    return new PrismaClient();
+  } else {
+    const newGlobalThis = globalThis as GlobalThisWithPrismaClient;
+    if (!newGlobalThis[prismaClientPropertyName]) {
+      newGlobalThis[prismaClientPropertyName] = new PrismaClient();
+    }
+    return newGlobalThis[prismaClientPropertyName];
   }
-  prisma = global.prisma
-}
+};
+const prisma = getPrismaClient();
 
-export default prisma
+export default Adapters.Prisma.Adapter({prisma});
